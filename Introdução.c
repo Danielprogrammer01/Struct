3af4bde5b5736920b5fc85ac41a15fe0ccd0038e
Fulano de Tal <fulanodetal@exemplo.br>
2023-11-30 13:26:28 -0300

    concluido

---
+++ Introdução.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <limits.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    return 0;
+}
